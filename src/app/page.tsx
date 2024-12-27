ebbd5de4f4686a0bbc5524733fa64e06c1392856
Sahil Gupta <guptas3067@gmail.com>
2024-12-27 17:42:48 +0530

    created publications page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function HomePage() {
   return (
     <>
       <div className="text-center border-1 border-green-500 ">
-        <h1>Home Page</h1>
+        {/* <h1>Home Page</h1> */}
         <p>Home Page Content</p>
         <div className="flex flex-col items-center justify-center">
           <Image src={primexopLogo} alt="primexopLogo" width={100} height={100} />
-          
+
         </div>
       </div>
     </>
