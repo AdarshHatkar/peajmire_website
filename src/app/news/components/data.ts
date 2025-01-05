363756b55bfeb12cc2a0d346e2f8b77ab9fa02d5
sarojbist <bistsaroj24@gmail.com>
2025-01-05 16:03:10 +0530

    add news section

---
+++ src/app/news/components/data.ts
@@ -0,0 +1,53 @@
+import news1 from "@assets/images/nai.png";
+import news2 from "@assets/images/carrer.jpg";
+import news3 from "@assets/images/numerical.jpg";
+import news4 from "@assets/images/interview.jpg";
+
+
+export const newsData = [
+    {
+        id: 1,
+        image: news1,
+        title: "Dr. Rumpf Named Senior Member of the National Academy of Inventors",
+        description:
+            "Dr. Raymond C. Rumpf, Ph.D., has been named senior member of the National Academy of Inventors (NAI). He is among the 124 foremost emerging academic inventors recognized by NAI’s Member Institutions for the 2024 class.",
+        tags: ["Career", "Recognition", "NAI"],
+        link: "#",
+    },
+    {
+        id: 2,
+        image: news2,
+        title: 'Dr. Rumpf Speaks at 24th International Conference-School "Advanced Materials and Technologies"',
+        description:
+            "Dr. Raymond Rumpf traveled to Kaunas University of Technology in Lithuania for the 24th International Conference themed 'Advanced Materials and Technologies'. He delivered three presentations on electromagnetics and photonics.",
+        tags: ["Career", "Conference", "Research"],
+        link: "#",
+    },
+    {
+        id: 3,
+        image: news3,
+        title: "Getting Started in Numerical Methods",
+        description:
+            "Dr. Rumpf emphasizes the importance of visualization in numerical computation. Visualization accelerates computation and enhances understanding, making it a key skill for success in numerical methods.",
+        tags: ["Wisdom", "Numerical Methods", "Education"],
+        link: "#",
+    },
+    {
+        id: 4,
+        image: news4,
+        title: "Cylindrical Lens Embedded Photonic Crystals Based on Self-Collimation",
+        description:
+            "A revolutionary lens design that operates without traditional refraction. This innovation holds potential for broader imaging applications and advanced beam manipulation.",
+        tags: ["Research", "Publications", "Photonics"],
+        link: "#",
+    },
+    {
+        id: 5,
+        image: news1,
+        title: "Dr. Rumpf Releases New Book on Computational Electromagnetics",
+        description:
+            "Dr. Rumpf’s book 'Electromagnetic and Photonic Simulation for the Beginner' introduces the FDFD method with easy-to-follow concepts and high-quality graphics for beginners.",
+        tags: ["Publications", "Book", "Electromagnetics"],
+        link: "#",
+    },
+];
---
+++ src/app/news/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Image from "next/image";
+import newsBanner from "@assets/images/news-banner.jpg";
+import { newsData } from "./components/data";
+import { IoFolderOpenOutline } from "react-icons/io5";
+
+import { useState } from "react";
+import Pagination from "./components/pagination";
+
+export default function NewsPage() {
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = 2;
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+    return (
+        <>
+            <div className="w-full bg-[#F6F8FA]">
+                <div className="relative">
+                    <Image src={newsBanner} alt="news banner" className="w-full h-60 object-cover"></Image>
+                    <div className="absolute inset-0 z-10 flex justify-center items-center">
+                        <h2 className="text-4xl font-semibold text-white">News</h2>{" "}
+                    </div>
+                </div>
+                <section className="py-12 xl:w-[1200px] xl:mx-auto">
+                    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                        {newsData.map((news) => (
+                            <div key={news.id} className="bg-white shadow-md rounded-md flex flex-col pb-4">
+                                <Image
+                                    src={news.image}
+                                    alt={news.title}
+                                    className="h-48 w-full object-cover mb-4 rounded-md cursor-pointer"
+                                />
+                                <h3 className="my-3 text-md ml-8">2023-3-20</h3>
+                                <h3 className="font-bold text-sm md:text-xl mb-2 px-6 hover:text-green-600 cursor-pointer">
+                                    {news.title}
+                                </h3>
+                                <p className="text-gray-700 text-sm md:text-lg flex-grow px-6 mt-3">
+                                    {news.description}
+                                </p>
+                                <div className="flex justify-start items-center px-6 my-4 gap-4">
+                                    <IoFolderOpenOutline className="text-green-600 text-lg" />
+                                    <div className="flex justify-start items-center gap-2 flex-wrap">
+                                        {news.tags.map((tag, index) => (
+                                            <span
+                                                key={index}
+                                                className="bg-green-100 text-green-600 cursor-pointer hover:text-blue-600 px-3 py-1 text-sm rounded-full font-medium mt-2"
+                                            >
+                                                {tag}
+                                            </span>
+                                        ))}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+                <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+            </div>
+        </>
+    );
+}
